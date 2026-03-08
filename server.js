820178beff73e9d6e547b15d66395ee71a134c8f
Baljeat_Les <hlan9693@gmail.com>
2026-03-08 17:11:14 +0700

    add root route

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.use("/", userRoutes);
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.send("CRUD USERS API is running");
+});
+
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
